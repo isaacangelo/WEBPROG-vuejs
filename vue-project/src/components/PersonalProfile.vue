285c28ea157d9600201f35f5023cdc3ed9a6dde2
isaacangelo <idestabillo@student.apc.edu.ph>
2025-01-31 12:58:20 +0800

    added personal profile

--- vue-project/src/components/PersonalProfile.vue
+++ vue-project/src/components/PersonalProfile.vue
@@ -1,65 +1,65 @@
 <template> 
-    <div class="container">
-        <header>
-            <h1>Isaac Angelo D. Estabillo</h1>
-        </header>
-        <main>
-            <section class="main-content">
-                <div>
-                    <h2>About Me</h2>
-                    <img src="https://scontent.fmnl25-3.fna.fbcdn.net/v/t39.30808-6/275339138_4891738160908762_559893741243629535_n.jpg" alt="Profile Picture">
-                    <p>Name: Isaac Angelo D. Estabillo</p>
-                    <p>Nickname: Icy</p>
-                </div>
-                <div>
-                    <h3>Education & Achievements</h3>
-                    <ul>
-                        <li>Rizal Technological University - JHS</li>
-                        <li>Asia Pacific College - SHS</li>
-                        <li>Asia Pacific College - College (current)</li>
-                        <li>Graduated SHS with High Honors</li>
-                    </ul>
-                </div>
-                <div>
-                    <h3>IT Experience</h3>
-                    <ul>
-                        <li>Basics of Python and Java</li>
-                    </ul>
-                </div>
-                <div>
-                    <h3>Hobbies & Interests</h3>
-                    <ul>
-                        <li>Basketball</li>
-                        <li>Volleyball</li>
-                        <li>Table Tennis</li>
-                        <li>Biking</li>
-                        <li>Binge watching series</li>
-                    </ul>
-                </div>
-                <div>
-                    <h3>Goals & Dreams</h3>
-                    <ul>
-                        <li>To be successful in the future while being happy and contented.</li>
-                    </ul>
-                </div>
-                <div>
-                    <h3>Picture Gallery</h3>
-                    <table>
-                        <tr>
-                            <td><img src="https://media.discordapp.net/attachments/893247075305816116/1311710226839244881/image.png" alt="Gallery Image 1"></td>
-                            <td><img src="https://media.discordapp.net/attachments/893247075305816116/1311711425152155669/d05d9b10-c5c7-4b7d-b833-f799fa3231e4.png" alt="Gallery Image 2"></td>
-                            <td><img src="https://media.discordapp.net/attachments/893247075305816116/1311711812038692915/01dee019-4b95-406e-a4a2-432dd8669054.png" alt="Gallery Image 3"></td>
-                            <td><img src="https://media.discordapp.net/attachments/893247075305816116/1311712088720281691/Z.png" alt="Gallery Image 4"></td>
-                        </tr>
-                    </table>
-                </div>
-            </section>
-        </main>
-        <footer>
-            &copy; 2025 Isaac Angelo D. Estabillo - All Rights Reserved
-        </footer>
-    </div>
+<div class="container">
+    <header>
+        <h1>Isaac Angelo D. Estabillo</h1>
+    </header>
+    <main>
+        <section class="main-content">
+            <div>
+                <h2>About Me</h2>
+                <img src="https://scontent.fmnl25-3.fna.fbcdn.net/v/t39.30808-6/275339138_4891738160908762_559893741243629535_n.jpg" alt="Profile Picture">
+                <p>Name: Isaac Angelo D. Estabillo</p>
+                <p>Nickname: Icy</p>
+            </div>
+            <div>
+                <h3>Education & Achievements</h3>
+                <ul>
+                    <li>Rizal Technological University - JHS</li>
+                    <li>Asia Pacific College - SHS</li>
+                    <li>Asia Pacific College - College (current)</li>
+                    <li>Graduated SHS with High Honors</li>
+                </ul>
+            </div>
+            <div>
+                <h3>IT Experience</h3>
+                <ul>
+                    <li>Basics of Python and Java</li>
+                </ul>
+            </div>
+            <div>
+                <h3>Hobbies & Interests</h3>
+                <ul>
+                    <li>Basketball</li>
+                    <li>Volleyball</li>
+                    <li>Table Tennis</li>
+                    <li>Biking</li>
+                    <li>Binge watching series</li>
+                </ul>
+            </div>
+            <div>
+                <h3>Goals & Dreams</h3>
+                <ul>
+                    <li>To be successful in the future while being happy and contented.</li>
+                </ul>
+            </div>
+            <div>
+                <h3>Picture Gallery</h3>
+                <table>
+                    <tr>
+                        <td><img src="https://media.discordapp.net/attachments/893247075305816116/1311710226839244881/image.png" alt="Gallery Image 1"></td>
+                        <td><img src="https://media.discordapp.net/attachments/893247075305816116/1311711425152155669/d05d9b10-c5c7-4b7d-b833-f799fa3231e4.png" alt="Gallery Image 2"></td>
+                        <td><img src="https://media.discordapp.net/attachments/893247075305816116/1311711812038692915/01dee019-4b95-406e-a4a2-432dd8669054.png" alt="Gallery Image 3"></td>
+                        <td><img src="https://media.discordapp.net/attachments/893247075305816116/1311712088720281691/Z.png" alt="Gallery Image 4"></td>
+                    </tr>
+                </table>
+            </div>
+        </section>
+    </main>
+    <footer>
+        &copy; 2025 Isaac Angelo D. Estabillo - All Rights Reserved
+    </footer>
+</div>
 </template>
 
-<script>/script>
+<script></script>
 <style></style>
